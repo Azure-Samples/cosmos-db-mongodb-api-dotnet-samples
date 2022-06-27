63f6ae447e10402a4a0496ae411c479e1ee2d6b5
Alex Wolf <alexwolf@microsoft.com>
2022-06-27 12:42:42 -0400

    update

--- 001-quickstart/Program.cs
+++ 001-quickstart/Program.cs
@@ -14,12 +14,13 @@ var client = new MongoClient(Environment.GetEnvironmentVariable("MONGO_CONNECTIO
 // <new_database> 
 // Database reference with creation if it does not already exist
 var db = client.GetDatabase("adventure");
+
 // </new_database>
 
-// <new_container> 
+// <new_collection> 
 // Container reference with creation if it does not alredy exist
 var _products = db.GetCollection<Product>("products");
-// </new_container>
+// </new_collection>
 
 // <new_item> 
 // Create new object and upsert (create or replace) to container
@@ -33,11 +34,27 @@ _products.InsertOne(new Product(
 // </new_item>
 
 // <read_item> 
-// Read item from container
-var product = _products.Find(p => p.Name.Contains("Yamba")).FirstOrDefault();
+// Read a single item from container
+var product = (await _products.FindAsync(p => p.Name.Contains("Yamba"))).FirstOrDefault();
+Console.WriteLine("Single product:");
+Console.WriteLine(product.Name);
 // </read_item>
 
 // <query_items> 
-// Create query using a SQL string and parameters
+// Read multiple items from container
+_products.InsertOne(new Product(
+    Guid.NewGuid().ToString(),
+    "gear-surf-surfboards",
+    "Sand Surfboard",
+    4,
+    false
+));
 
+var products = _products.AsQueryable().Where(p => p.Category == "gear-surf-surfboards");
+
+Console.WriteLine("Multiple products:");
+foreach (var prod in products)
+{
+    Console.WriteLine(prod.Name);
+}
 // </query_items>
