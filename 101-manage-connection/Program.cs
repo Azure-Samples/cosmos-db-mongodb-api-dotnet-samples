06861b32b12c4a7afa99e5adbc945abde6aa3ff0
alexwolfmsft <93200798+alexwolfmsft@users.noreply.github.com>
2022-10-17 09:44:41 -0400

    Update Program.cs

--- 101-manage-connection/Program.cs
+++ 101-manage-connection/Program.cs
@@ -9,7 +9,7 @@ using MongoDB.Driver;
 
 // <client_credentials> 
 // New instance of CosmosClient class
-var client = new MongoClient(Environment.GetEnvironmentVariable("MONGO_CONNECTION"));
+var client = new MongoClient(Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING"));
 
 var settings = client.Settings;
 
